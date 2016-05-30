ca3a8fb556d6ef10682ec9a7ed947983531accd2
EDA <eda@mac-mini-3.local>
2016-05-30 18:06:38 +1200

    array of objects

--- readFile.js
+++ readFile.js
@@ -1,21 +1,37 @@
 var fs = require('fs');
 var path = require('path');
 
+var myObject = {}
+var emptyArray = []
+
 fs.readFile('./data/female.txt', function (err, buffer) {
   if (err) {
     throw err;
   }
   var holdString = buffer.toString()
 
-  var fields = holdString.split(' ').join('')
+  holdString = holdString.split(' ').join('')
 
-  fields = fields.replace(/[0-9]/g, '');
-  fields = fields.replace(/[^\w\s]/gi, '')
-  var object = JSON.parse(fields)
-  console.log(object)
-    // var trimmed = fields.trim()
-    // console.log(trimed)
+  holdString = holdString.replace(/[0-9]/g, '');
+
+  holdString = holdString.replace(/[^\w\s]/gi, '')
+
+  someText = holdString.replace(/(\r\n|\n|\r)/gm, " ");
+
+  var myArray = someText.split(" ")
+
+
+  for (var i = 0; i < myArray.length; i++) {
+    emptyArray.push({
+        firstName: myArray[i]
+      })
+      // myArray[i].firstName =
+      // console.log(myArray[i])
+      // myObject.firstName = myArray[i]
+  }
+  console.log(emptyArray);
 })
+
 
 //
 // fs.readFile(path, function (err, buffer) {
